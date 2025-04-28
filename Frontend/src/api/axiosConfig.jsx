bd104257808e3cd3e54f65ac5c256cbabc8ecfab
Anshi-01 <anshisrivastav16@gmail.com>
2025-04-28 15:40:01 +0530

    changed

--- Frontend/src/api/axiosConfig.jsx
+++ Frontend/src/api/axiosConfig.jsx
@@ -1,7 +1,7 @@
     import axios  from "axios";
 
     const instance = axios.create({
-        baseURL: "https://hotelapp-1-38xj.onrender.com/api",
+        baseURL: "https://hotelapp-2.onrender.com",
         withCredentials:true
     })
 
